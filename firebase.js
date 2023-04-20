ada02830b10861c24e23a35a947abfec0e89d3d1
ShamasadAlam <shamsada@builtvisor.com>
2023-04-20 20:52:47 +0530

    amazon2.0

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD6pp17A8OEst7zM_ETi5gINk4iwe-DEow",
+    authDomain: "amazn-2d05d.firebaseapp.com",
+    projectId: "amazn-2d05d",
+    storageBucket: "amazn-2d05d.appspot.com",
+    messagingSenderId: "1073963482694",
+    appId: "1:1073963482694:web:5df9d10ade4a545b239447",
+    measurementId: "G-TZ0QN7G5XJ"
+  };
--- src/components/Products.js
+++ src/components/Products.js
@@ -2,28 +2,39 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { StarIcon } from "@heroicons/react/24/solid";
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Products({ id, title, price, description, category, image }) {
+  const disptach = useDispatch();
   const [rating] = useState(
     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   );
 
   const [hasPrime] = useState(Math.random() < 0.5);
+
+  const addItemToBasket = () =>{
+    const product  = {id, title, price,rating, description, category, image,hasPrime};
+
+    // sending the product as an action to the REDUX Store.. the basket slice 
+    disptach(addToBasket(product))
+  }
+
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
       <p className="absolute top-2 right-2 text-xs italic text-gray-400">
         {category}
       </p>
-      <Image src={image} height={200} width={200} objectFit="contain" />
+      <Image src={image} height={200} width={200} objectFit="contain" alt="img" />
       <h4 className="my-3">{title}</h4>
       <div className="flex">
         {Array(rating)
           .fill()
           .map((_, i) => (
-            <StarIcon className="h-5 text-yellow-500" />
+            <StarIcon className="h-5 text-yellow-500" key={""} />
           ))}
       </div>
       <p className="text-xs my-2 line-clamp-2">{description}</p>
@@ -32,11 +43,11 @@ function Products({ id, title, price, description, category, image }) {
       </div>
       {hasPrime && (
         <div className="flex items-center space-x-2 -mt-5">
-          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+          <img className="w-12" src="https://links.papareact.com/fdw" alt="img" />
           <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
       )}
-      <button className="mt-auto button">Add to Basket</button>
+      <button onClick={addItemToBasket} className="mt-auto button">Add to Basket</button>
     </div>
   );
 }
